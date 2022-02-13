1abed36a85a3f92350dfd7159b4cf232c231db4f
Reload0213 <85751554+Reload0213@users.noreply.github.com>
2022-02-13 20:18:35 +0900

    commit test
    commit test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+
 
 function App() {
+
+  let posts = '오늘의 식단';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="blogMenu">
+        <div className="blogTitle">세미 blog</div>
+      </div>
+      <h4> { posts } </h4>
     </div>
   );
 }
