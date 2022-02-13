c190fc9f9aed14182e11abbe01818b42c64baa00
Reload0213 <85751554+Reload0213@users.noreply.github.com>
2022-02-13 23:33:43 +0900

    usestate, 이벤트 활용 추가 후 커밋 테스트
    usestate, 이벤트 활용 추가 후 커밋 테스트

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// eslint = 잘못된 코딩 관습, 문법 잡아주는 경고
+/* eslint-disable */ 
+
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -5,13 +9,36 @@ import "./App.css";
 
 function App() {
 
-  let posts = '오늘의 식단';
+  // let posts = '오늘의 식단';
+  // let [ blogTitle, CgBlogTitle ] = useState('오늘의 식단');
+  let [ blogTitle, CgBlogTitle ] = useState(['오늘의 식단', 'OOTD', '오늘의 공부']);
+  let [likeCnt, CgLikeCnt ]  = useState(0);
+
+  let ChangeTitle = () => {
+    let newTitle  = [...blogTitle];
+      newTitle[0] = '오늘의 간식';
+        CgBlogTitle( newTitle )
+  }
+
+
   return (
     <div className="App">
       <div className="blogMenu">
         <div className="blogTitle">세미 blog</div>
       </div>
-      <h4> { posts } </h4>
+      <div className="blogList">
+      <h4> { blogTitle[0] } <span onClick={ () => { CgLikeCnt(likeCnt+1) }}> 👍 </span> { likeCnt } </h4>
+      <p>00월 00일</p>
+      </div>
+      <button onClick={ () => { ChangeTitle() } }> 오늘은 간식을 먹었습니다.</button>
+      <div className="blogList">
+      <h4> { blogTitle[1] } </h4>
+      <p>00월 00일</p>
+      </div>
+      <div className="blogList">
+      <h4> { blogTitle[2] } </h4>
+      <p>00월 00일</p>
+      </div>
     </div>
   );
 }
